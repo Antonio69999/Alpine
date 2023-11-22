cf5c828acbb24d1aae286890fa61402f4d352ce3
Antonio69999 <antoine.chabeauti@gmail.com>
2023-11-22 14:09:54 +0100

    can change color but can't go arround models

--- src/components/Color/Color.jsx
+++ src/components/Color/Color.jsx
@@ -1,85 +1,34 @@
 import { useSelector, useDispatch } from "react-redux";
 import { setSelectedColor, setSelectedCar } from "../../features/modeleSlice";
-//IMAGES PURE
-import pureblanchefront from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-1.jpg";
-import pureblancheback from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-2.jpg";
-import pureblancheleft from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-3.jpg";
-import pureblancheright from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-4.jpg";
-
-import purenoirfront from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-1.jpg";
-import purenoirback from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-2.jpg";
-import purenoirleft from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-3.jpg";
-import purenoirright from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-4.jpg";
-
-import purebleufront from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-1.jpg";
-import purebleuback from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-2.jpg";
-import purebleuleft from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-3.jpg";
-import purebleuright from "../../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-4.jpg";
-
-//IMAGE LEGENDE
-import legendeblancheback from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-1.jpg";
-import legendeblancheright from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-2.jpg";
-import legendeblancheleft from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-3.jpg";
-import legendeblanchefront from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-4.jpg";
-
-import legendebleuback from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-1.jpg";
-import legendebleuright from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-2.jpg";
-import legendebleuleft from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-3.jpg";
-import legendebleufront from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-4.jpg";
-
-import legendebnoireback from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-1.jpg";
-import legendebnoireright from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-2.jpg";
-import legendebnoireleft from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-3.jpg";
-import legendebnoirefront from "../../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-4.jpg";
+import { useState, useEffect } from "react";
 
 const Color = () => {
   const selectedCar = useSelector((state) => state.cars.selectedCar);
-  const colors = useSelector((state) => state.cars.colors);
   const selectedColor = useSelector((state) => state.cars.selectedColor);
   const dispatch = useDispatch();
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  useEffect(() => {
+    console.log("selectedCar:", selectedCar);
+    console.log("selectedColor:", selectedColor);
+  }, [selectedCar, selectedColor]);
+
   const handleColorChange = (color) => {
     dispatch(setSelectedColor(color));
-    const carImage = color.image;
-    dispatch(setSelectedCar({ ...selectedCar, img: carImage })); //update car
+    setCurrentImageIndex(0); // Reset image index when color changes
   };
 
-  const PureImages = [
-    //Blanc
-    pureblanchefront,
-    pureblancheback,
-    pureblancheleft,
-    pureblancheright,
-    //Noir
-    purenoirfront,
-    purenoirback,
-    purenoirleft,
-    purenoirright,
-    //Bleu
-    purebleufront,
-    purebleuback,
-    purebleuleft,
-    purebleuright,
-  ];
+  const nextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % selectedColor.images.length
+    );
+  };
 
-  const LegendeImage = [
-    //Blanc
-    legendeblancheback,
-    legendeblancheright,
-    legendeblancheleft,
-    legendeblanchefront,
-
-    //BLEU
-    legendebleuback,
-    legendebleuright,
-    legendebleuleft,
-    legendebleufront,
-    //Noire
-    legendebnoireback,
-    legendebnoireright,
-    legendebnoireleft,
-    legendebnoirefront,
-  ];
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? selectedColor.images.length - 1 : prevIndex - 1
+    );
+  };
 
   return (
     <>
@@ -88,40 +37,35 @@ const Color = () => {
       </div>
 
       <div className="container flex justify-center items-center mt-8">
-        {selectedCar.name === "A110 Pure" && (
-          <div className="max-w-full overflow-x-auto">
-            <div className="flex space-x-4">
-              {PureImages.map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`Car View ${index + 1}`}
-                  className={`w-full ${index === 0 ? "" : "hidden"}`}
-                />
-              ))}
-            </div>
-          </div>
-        )}
-
-        {selectedCar.name === "A110 Légende" && (
-          <div className="max-w-full overflow-x-auto">
-            <div className="flex space-x-4">
-              {LegendeImage.map((image, index) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt={`Car View ${index + 1}`}
-                  className={`w-full ${index === 0 ? "" : "hidden"}`}
-                />
-              ))}
-            </div>
+        {selectedCar && selectedColor && (
+          <div className="max-w-full overflow-hidden">
+            {Object.values(selectedColor.images).map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Car View ${index + 1}`}
+                className={`w-full ${
+                  index === currentImageIndex ? "" : "hidden"
+                }`}
+              />
+            ))}
           </div>
         )}
       </div>
 
+      {/* Navigation button */}
+      <div className="flex justify-center mt-4">
+        <button onClick={prevImage} className="mr-2 px-4 py-2 bg-gray-300">
+          Previous
+        </button>
+        <button onClick={nextImage} className="px-4 py-2 bg-gray-300">
+          Next
+        </button>
+      </div>
+
       <div className="mt-8">
         <h2 className="text-xl mb-2">Available Colors:</h2>
-        {colors.map((color) => (
+        {selectedCar.colorOptions.map((color) => (
           <div key={color.id} className="mb-4">
             <p className="text-lg">{color.name}</p>
             <button
--- src/features/modeleSlice.jsx
+++ src/features/modeleSlice.jsx
@@ -5,10 +5,38 @@ import legende from "../assets/Images/configurateur/modele/selection/legende.png
 import pure from "../assets/Images/configurateur/modele/selection/pure.png";
 import D from "../assets/Images/logoD.png";
 
-//COULEURS
-import blanc from "../assets/Images/configurateur/couleurs/selection/blanc.jpg";
-import bleu from "../assets/Images/configurateur/couleurs/selection/bleu.jpg";
-import noir from "../assets/Images/configurateur/couleurs/selection/noir.jpg";
+//IMAGES PURE
+import pureblanchefront from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-1.jpg";
+import pureblancheback from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-2.jpg";
+import pureblancheleft from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-3.jpg";
+import pureblancheright from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_blanche-jante_standard-4.jpg";
+
+import purenoirfront from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-1.jpg";
+import purenoirback from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-2.jpg";
+import purenoirleft from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-3.jpg";
+import purenoirright from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_noire-jante_standard-4.jpg";
+
+import purebleufront from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-1.jpg";
+import purebleuback from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-2.jpg";
+import purebleuleft from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-3.jpg";
+import purebleuright from "../assets/Images/configurateur/modele/pure/modele_pure-couleur_bleu-jante_standard-4.jpg";
+
+//IMAGE LEGENDE
+import legendeblancheback from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-1.jpg";
+import legendeblancheright from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-2.jpg";
+import legendeblancheleft from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-3.jpg";
+import legendeblanchefront from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_blanc-jante_legende-4.jpg";
+
+import legendebleuback from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-1.jpg";
+import legendebleuright from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-2.jpg";
+import legendebleuleft from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-3.jpg";
+import legendebleufront from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_bleu-jante_legende-4.jpg";
+
+import legendebnoireback from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-1.jpg";
+import legendebnoireright from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-2.jpg";
+import legendebnoireleft from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-3.jpg";
+import legendebnoirefront from "../assets/Images/configurateur/modele/legende/modele_legende-couleur_noir-jante_legende-4.jpg";
+
 
 const initialState = {
   models: [
@@ -16,27 +44,42 @@ const initialState = {
       name: "A110 Pure",
       price: "54 700",
       rent: "650",
-      img: legende,
+      img: pure,
       energetique: D,
       id: 1,
       colorOptions: [
         {
           name: "Peinture opaque Blanc Glacier",
-          img: pureblanchefront,
+          images: {
+            front: pureblanchefront,
+            back: pureblancheback,
+            left: pureblancheleft,
+            right: pureblancheright,
+          },
           price: "0",
           default: true,
           id: 1,
         },
         {
           name: "Teinte spéciale Bleu Alpine",
-          img: purebleufront,
+          images: {
+            front: purebleufront,
+            back: purebleuback,
+            left: purebleuleft,
+            right: purebleuright,
+          },
           price: "1800",
           default: false,
           id: 2,
         },
         {
           name: "Teinte métallisée Noir Profond",
-          img: purenoirfront,
+          images: {
+            front: purenoirfront,
+            back: purenoirback,
+            left: purenoirleft,
+            right: purenoirright,
+          },
           price: "840",
           default: false,
           id: 3,
@@ -48,27 +91,42 @@ const initialState = {
       name: "A110 Légende",
       price: "58 500",
       rent: "760",
-      img: pure,
+      img: legende,
       energetique: D,
       id: 2,
       colorOptions: [
         {
           name: "Peinture opaque Blanc Glacier",
-          img: legendeblanchefront,
+          images: {
+            front: legendeblanchefront,
+            back: legendeblancheback,
+            left: legendeblancheleft,
+            right: legendeblancheright,
+          },
           price: "0",
           default: true,
           id: 4,
         },
         {
           name: "Teinte spéciale Bleu Alpine",
-          img: legendebbleufront,
+          images: {
+            front: legendebleufront,
+            back: legendebleuback,
+            left: legendebleuleft,
+            right: legendebleuright,
+          },
           price: "1800",
           default: false,
           id: 5,
         },
         {
           name: "Teinte métallisée Noir Profond",
-          img: legendebnoirfront,
+          images: {
+            front: legendebnoirefront,
+            back: legendebnoireback,
+            left: legendebnoireleft,
+            right: legendebnoireright,
+          },
           price: "840",
           default: false,
           id: 6,
@@ -94,9 +152,18 @@ export const modelSlice = createSlice({
     },
     setSelectedCar: (state, action) => {
       state.selectedCar = action.payload;
+      state.selectedColor = null; // Reset selected color when changing the car
     },
     setSelectedColor: (state, action) => {
-      state.selectedColor = action.payload;
+      const selectedModel = state.models.find(
+        (model) => model.id === state.selectedCar.id
+      );
+
+      state.selectedColor = selectedModel
+        ? selectedModel.colorOptions.find(
+            (colorOption) => colorOption.id === action.payload.id
+          )
+        : null;
     },
   },
 });
