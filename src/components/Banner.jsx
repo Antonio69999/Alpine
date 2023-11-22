755cbebb5bd74bb887f721d618b243da7afbac21
Antonio69999 <antoine.chabeauti@gmail.com>
2023-11-22 15:56:22 +0100

    banner racers

---
+++ src/components/Banner.jsx
@@ -0,0 +1,27 @@
+const Banner = () => {
+    return (
+      <div className="bg-gray-200">
+        <div className="max-w-screen-md mx-auto p-6 text-center">
+          <h1 className="text-3xl font-bold mb-4">MADE BY RACERS</h1>
+          <p className="mb-4">
+            En 1955, Jean Rédélé, entrepreneur passionné, fonde Alpine sur trois
+            piliers : agilité, élégance, esprit de compétition.{" "}
+          </p>
+          <p className="mb-4">
+            Conçue par des pilotes pour ceux qui veulent maîtriser l’asphalte, la
+            légende Alpine s’est érigée à travers des victoires mythiques, et un
+            engagement sportif sans limite.
+          </p>
+          <p className="mb-4">
+            Depuis 2016, à travers notre nouvelle gamme A 110, nous continuons à
+            gravir tous les échelons du sport automobile. Demain, nous voulons
+            réinventer la sportivité automobile avec des technologies innovantes
+            issues de la Formule 1, pour des pilotes passionnés.
+          </p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Banner;
+  
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import CarouselWithContent from "../components/Carousel";
 import NavbarDefault from "../components/Navbar";
+import Banner from "../components/Banner";
 
 const Header = () => {
   return (
     <>
       <NavbarDefault />
       <CarouselWithContent />
+      <Banner/>
     </>
   );
 };
